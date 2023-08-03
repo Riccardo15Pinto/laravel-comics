e4e00d8df65c0e4df61b45620349c339ade2bbb5
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-03 16:42:45 +0200

    first commit

---
+++ config/voices.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    [
+        "text" => "Characters",
+        "route" => "#",
+    ],
+    [
+        'text' => 'Comics',
+        'route' => '#',
+    ],
+    [
+        'text' => 'Movies',
+        'route' => '#',
+    ],
+    [
+        'text' => 'TV',
+        'route' => '#',
+    ],
+    [
+        'text' => 'Collectibles',
+        'route' => '#',
+    ],
+    [
+        'text' => 'Videos',
+        'route' => '#',
+    ],
+    [
+        'text' => 'Fans',
+        'route' => '#',
+    ],
+    [
+        'text' => 'News',
+        'route' => '#',
+    ],
+    [
+        'text' => 'Shop',
+        'route' => '#',
+    ],
+
+];
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+
+    <div>
+
+        <figure>
+            <img src="{{@Vite::asset('resources/img/dc-logo.png')}}" alt="">
+        </figure>
+        <ul>
+            @foreach($voices as $voice)
+            <li>
+
+                <a href="">{{$voice['text']}}</a>
+            </li>
+            @endforeach
+        </ul>
+    </div>
+
+</header>
