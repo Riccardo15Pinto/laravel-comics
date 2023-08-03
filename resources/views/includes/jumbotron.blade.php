06e4e66aec41cfe94c75a016f1adc3c500866806
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-03 17:41:18 +0200

    create jumbo

--- resources/views/includes/jumbotron.blade.php
+++ resources/views/includes/jumbotron.blade.php
@@ -1,18 +1,18 @@
 <section class="jumbo-section">
     <div class="top-jumbo">
+        <img src="{{ @Vite::asset('resources/img/jumbotron.jpg') }}" alt="">
         <h2>Current Series</h2>
     </div>
     <div class="bottom-jumbo">
         <div class="container-card">
-            @foreach($comics as $comic)
+            @foreach ($comics as $comic)
+                <div class="card">
+                    <figure>
+                        <img src="{{ $comic['thumb'] }}" alt="">
+                    </figure>
 
-            <div class="card">
-                <figure>
-                    <img src="{{ $comic['thumb']}}" alt="">
-                </figure>
-
-                <h3>{{ $comic['series']}}</h3>
-            </div>
+                    <h3>{{ $comic['series'] }}</h3>
+                </div>
             @endforeach
 
         </div>
@@ -23,14 +23,13 @@
 
     <div>
         <ul>
-            @foreach($buys as $buy)
-
-            <li>
-                <figure>
-                    <img src="{{@Vite::asset($buy['pic'])}}" alt="">
-                </figure>
-                <span>{{ $buy['name'] }}</span>
-            </li>
+            @foreach ($buys as $buy)
+                <li>
+                    <figure>
+                        <img src="{{ @Vite::asset("resources/img/{$buy['pic']}") }}" alt="">
+                    </figure>
+                    <span>{{ $buy['name'] }}</span>
+                </li>
             @endforeach
 
         </ul>
