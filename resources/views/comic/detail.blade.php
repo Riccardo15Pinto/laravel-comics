fc92f0de44f1769c8241783be4cde7f3e09779e3
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-21 15:15:59 +0200

    add section item

--- resources/views/comic/detail.blade.php
+++ resources/views/comic/detail.blade.php
@@ -2,6 +2,25 @@
 
 @section('title', 'COMIC')
 
+@section('item')
+    <div class="flag-detail">
+        <figure>
+            <img src="{{ $product['thumb'] }}" alt="">
+        </figure>
+    </div>
+@endsection
+
+
 @section('main')
-    <h1>{{ $product['title'] }}</h1>
+    <section class="details">
+
+        <div class="info-detail">
+            <h1>{{ $product['title'] }}</h1>
+            <p>{{ $product['description'] }}</p>
+        </div>
+
+        <figure>
+            <img src="{{ @Vite::asset('resources/img/adv.jpg') }}" alt="">
+        </figure>
+    </section>
 @endsection
