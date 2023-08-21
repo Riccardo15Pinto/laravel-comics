fe6ef87a560996dd1ec84ae2d6b7af2626b8a0c8
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-21 17:06:04 +0200

    set detail

--- resources/views/comic/detail.blade.php
+++ resources/views/comic/detail.blade.php
@@ -14,13 +14,100 @@
 @section('main')
     <section class="details">
 
-        <div class="info-detail">
-            <h1>{{ $product['title'] }}</h1>
-            <p>{{ $product['description'] }}</p>
+        <div class="detail-container">
+
+            <div class="col">
+
+                <div class="info-detail">
+
+                    <h1>{{ $product['title'] }}</h1>
+
+                    <div id="info-avaible">
+
+                        <div>
+                            <h4>U.S. Price:
+                                <span>
+                                    {{ $product['price'] }}
+                                </span>
+                            </h4>
+                            <h4>Avaible</h4>
+                        </div>
+                        <div>
+                            <h4>Check Avaiblety</h4>
+                        </div>
+                    </div>
+
+                    <p>{{ $product['description'] }}</p>
+                </div>
+            </div>
+            <div class="col">
+                <h1>Advertisment</h1>
+
+                <figure>
+                    <img src="{{ @Vite::asset('resources/img/adv.jpg') }}" alt="">
+                </figure>
+            </div>
         </div>
 
-        <figure>
-            <img src="{{ @Vite::asset('resources/img/adv.jpg') }}" alt="">
-        </figure>
+        <div class="detail-product">
+            <div class="detail-product-container">
+
+                <div>
+                    <h2>Talent</h2>
+                    <ul>
+                        <li>
+                            <h3>Art by:</h3>
+                            <div>
+                                @foreach ($product['artists'] as $artist)
+                                    <span>{{ $artist }} @if (!$loop->last)
+                                            ,
+                                        @else
+                                            .
+                                        @endif
+                                    </span>
+                                @endforeach
+                            </div>
+                        </li>
+
+                        <li>
+                            <h3>Written by:</h3>
+                            <div>
+                                @foreach ($product['writers'] as $artist)
+                                    <span>{{ $artist }} @if (!$loop->last)
+                                            ,
+                                        @else
+                                            .
+                                        @endif
+                                    </span>
+                                @endforeach
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+
+                <div>
+                    <h2>Specs</h2>
+                    <ul>
+                        <li>
+                            <h3>Series:</h3>
+                            <h3>{{ $product['series'] }}</h3>
+                        </li>
+
+                        <li>
+                            <h3>U.S.Price:</h3>
+                            <h3>{{ $product['price'] }}</h3>
+                        </li>
+
+                        <li>
+                            <h3>On Sale Date:</h3>
+                            <h3>{{ $product['sale_date'] }}</h3>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
     </section>
 @endsection
